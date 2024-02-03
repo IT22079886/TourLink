6493481308882d4ab621408739adbdfbfc1bd050
IT22079886 <128355776+IT22079886@users.noreply.github.com>
2024-02-03 22:31:56 +0530

    create the project

---
+++ Travel-agency-management-system/src/main/java/com/example/travelagencymanagementsystem/TravelAgencyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelagencymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyManagementSystemApplication.class, args);
+	}
+
+}
